db3b15bc9dced259d0e9aa3a6bd54a1587283dc8
dustynaugust <dustyn.august@gmail.com>
2016-01-22 19:11:01 -0800

    Attempting to display in Collection View
    Decided to add a second view controller to handle the CollectionView.
    Still not working.
    
    Crash when switching pages.

--- MovieViewer/MoviesCollectionViewController.swift
+++ MovieViewer/MoviesCollectionViewController.swift
@@ -1,59 +1,33 @@
 //
-//  MoviesViewController.swift
+//  MoviesCollectionViewController.swift
 //  MovieViewer
 //
-//  Created by ccsfcomputers on 1/19/16.
+//  Created by Dustyn August on 1/22/16.
 //  Copyright © 2016 ccsfcomputers. All rights reserved.
 //
-
-
-//
-// Don't forget to check out the guides on courses.codepath.com
-//
-// Network error:
-//          If I have the network error message hidden then I can simply check if there is a network error
-//          and then unhide the message if there is. When the error gets fixed hide it again.
-//
-//
-// Collection View:
-//          Probably want to swipe to see a new page for collection view.
-//          Gid vs Lines
-//
-//
-// Search for a movie:
-//          UISearchBar
-//
-// Image Fade:
-//
-//
-// Custom UI:
-//
-//
-
-
 
 import UIKit
 import AFNetworking
 import MBProgressHUD
 
-class MoviesViewController: UIViewController, UITableViewDataSource, UITableViewDelegate {
+class MoviesCollectionViewController: UIViewController, UICollectionViewDataSource{
+
     
-    @IBOutlet weak var tableView: UITableView!
-    
-    
+    @IBOutlet weak var collectionView: UICollectionView!
     
     var movies: [NSDictionary]?
 
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         
-        tableView.dataSource = self
-        tableView.delegate = self
         
+        
+        collectionView.dataSource = self
         
         let refreshControl = UIRefreshControl()
         refreshControl.addTarget(self, action: "refreshControlAction:", forControlEvents: UIControlEvents.ValueChanged)
-        tableView.insertSubview(refreshControl, atIndex: 0)
+        collectionView.insertSubview(refreshControl, atIndex: 0)
         
         let apiKey = "a07e22bc18f5cb106bfe4cc1f83ad8ed"
         let url = NSURL(string:"https://api.themoviedb.org/3/movie/now_playing?api_key=\(apiKey)")
@@ -65,7 +39,7 @@ class MoviesViewController: UIViewController, UITableViewDataSource, UITableView
         )
         
         MBProgressHUD.showHUDAddedTo(self.view, animated: true)
-
+        
         
         let task : NSURLSessionDataTask = session.dataTaskWithRequest(request,
             completionHandler: { (dataOrNil, response, error) in
@@ -75,13 +49,14 @@ class MoviesViewController: UIViewController, UITableViewDataSource, UITableView
                             NSLog("response: \(responseDictionary)")
                             
                             MBProgressHUD.hideHUDForView(self.view, animated: true)
-
+                            
                             self.movies = responseDictionary["results"] as! [NSDictionary]
-                            self.tableView.reloadData()
+                            self.collectionView.reloadData()
                     }
                 }
         });
         task.resume()
+
 
         // Do any additional setup after loading the view.
     }
@@ -91,53 +66,27 @@ class MoviesViewController: UIViewController, UITableViewDataSource, UITableView
         // Dispose of any resources that can be recreated.
     }
     
-
-    func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+    
+    
+    func collectionView(collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
         
         if let movies = movies {
             return movies.count
         } else {
             return 0
         }
-        
     }
     
-    func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
+    func collectionView(collectionView: UICollectionView, cellForItemAtIndexPath indexPath: NSIndexPath) -> UICollectionViewCell {
+        let cell = collectionView.dequeueReusableCellWithReuseIdentifier("MovieCollectionViewCell", forIndexPath: indexPath) as! MovieCollectionViewCell
         
-        let cell = tableView.dequeueReusableCellWithIdentifier("MovieCell", forIndexPath: indexPath) as! MovieCell
         
         
-        let movie = movies![indexPath.row]
-        let title = movie["title"] as! String
-        let overview = movie ["overview"] as! String
-        let baseUrl = "http://image.tmdb.org/t/p/w500"
-        
-        let posterPath = movie["poster_path"] as! String
-        
-        let imageUrl = NSURL(string: baseUrl + posterPath)
-        
-        cell.titleLabel.text = title
-        cell.overviewLabel.text = overview
-        cell.posterView.setImageWithURL(imageUrl!)
-        
-       
-        print ("row \(indexPath.row)")
         return cell
     }
+    
+    
 
-    
-    func refreshControlAction(refreshControl: UIRefreshControl) {
-        
-        self.tableView.reloadData()
-        refreshControl.endRefreshing()
-    }
-    
-    
-    
-  
-    
-    
-    
     /*
     // MARK: - Navigation
 
